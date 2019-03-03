b573443fcb90def1a09ba602bba17042f6482e0f
Yuna Luzi <junaluzi@gmail.com>
2019-03-03 17:33:57 -0500

    week 5 assignment problem 2 done

--- Week 5 - Object Oriented Programming/ProblemSet5/README.md
+++ Week 5 - Object Oriented Programming/ProblemSet5/README.md
@@ -75,3 +75,16 @@ A reminder from the introduction page - characters such as the space character, 
 
 Fill in the `apply_shift(self, shift)` method of the `Message` class. You may find it easier to use `build_shift_dict(self, shift)`. Remember that spaces and punctuation should not be changed by the cipher.
 
+## Problem 2 - PlaintextMessage ##
+
+For this problem, the graders will use our implementation of the `Message` class, so don't worry if you did not get the previous parts correct.
+
+`PlaintextMessage` is a subclass of `Message` and has methods to encode a string using a specified shift value. Our class will always create an encoded version of the message, and will have methods for changing the encoding.
+
+Implement the methods in the class `PlaintextMessage` according to the specifications in ps6.py. The methods you should fill in are:
+
+* `__init__(self, text, shift)`: Use the parent class constructor to make your code more concise.
+* The getter method `get_shift(self)`
+* The getter method `get_encrypting_dict(self)`: This should return a COPY of self.encrypting_dict to prevent someone from mutating the original dictionary.
+* The getter method `get_message_text_encrypted(self)`
+* `change_shift(self, shift)`: Think about what other methods you can use to make this easier. It shouldn’t take more than a couple lines of code.
--- Week 5 - Object Oriented Programming/ProblemSet5/ps6.py
+++ Week 5 - Object Oriented Programming/ProblemSet5/ps6.py
@@ -164,7 +164,10 @@ class PlaintextMessage(Message):
         Hint: consider using the parent class constructor so less 
         code is repeated
         '''
-        pass #delete this line and replace with your code here
+        Message.__init__(self, text)
+        self.shift = shift
+        self.encrypting_dict = Message.build_shift_dict(self, shift)
+        self.message_text_encrypted = Message.apply_shift(self, shift)
 
     def get_shift(self):
         '''
@@ -172,7 +175,7 @@ class PlaintextMessage(Message):
         
         Returns: self.shift
         '''
-        pass #delete this line and replace with your code here
+        return self.shift
 
     def get_encrypting_dict(self):
         '''
@@ -180,7 +183,8 @@ class PlaintextMessage(Message):
         
         Returns: a COPY of self.encrypting_dict
         '''
-        pass #delete this line and replace with your code here
+        encrypting_dict_copy = self.encrypting_dict.copy()
+        return encrypting_dict_copy
 
     def get_message_text_encrypted(self):
         '''
@@ -188,7 +192,7 @@ class PlaintextMessage(Message):
         
         Returns: self.message_text_encrypted
         '''
-        pass #delete this line and replace with your code here
+        return self.message_text_encrypted
 
     def change_shift(self, shift):
         '''
@@ -201,7 +205,9 @@ class PlaintextMessage(Message):
 
         Returns: nothing
         '''
-        pass #delete this line and replace with your code here
+        self.shift = shift
+        self.encrypting_dict = Message.build_shift_dict(self, shift)
+        self.message_text_encrypted = Message.apply_shift(self, shift)
 
 
 class CiphertextMessage(Message):
